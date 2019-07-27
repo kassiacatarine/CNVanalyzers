17125fe37401822c305e07084fea588904620e86
Kássia Catarine <kassia_catarine.15@hotmail.com>
2019-07-27 10:48:51 -0300

    Adding vignettes documentation
    Signed-off-by: Kássia Catarine <kassia_catarine.15@hotmail.com>

--- CNVanalyzers/R/hello.R
+++ CNVanalyzers/R/hello.R
@@ -1,6 +1,6 @@
 # Hello, world!
 #
-# This is an example function named 'hello' 
+# This is an example function named 'hello'
 # which prints 'Hello, world!'.
 #
 # You can learn more about package authoring with RStudio at:
@@ -10,7 +10,7 @@
 # Some useful keyboard shortcuts for package authoring:
 #
 #   Build and Reload Package:  'Ctrl + Shift + B'
-#   Check Package:             'Ctrl + Shift + E'
+#   Check Package:             'Ctrl + Shift   + E'
 #   Test Package:              'Ctrl + Shift + T'
 
 hello <- function() {
